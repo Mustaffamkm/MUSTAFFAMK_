5e6efc32c9fb0a902631b928ee480b232c1bb693
Mustaffamkm <kimongemustaffa@gmail.com>
2025-01-19 14:29:35 +0300

    Update mustts.js

--- plugins/eztts.js
+++ plugins/eztts.js
@@ -24,7 +24,7 @@ async (conn, mek, m, { from, q, reply }) => {
         const response = await axios.get(url);
         const translation = response.data.responseData.translatedText;
 
-        const translationMessage = `> *✧FREDI MD✧ TRANSLATION*
+        const translationMessage = `> *✧MUSTAFFA MK✧ TRANSLATION*
 
 > 🔤 *Original*: ${textToTranslate}
 
