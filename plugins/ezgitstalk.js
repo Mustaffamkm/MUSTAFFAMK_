9f44a69307e69b7271f7416d924075564044b19a
Mustaffamkm <kimongemustaffa@gmail.com>
2025-01-19 11:01:11 +0300

    Update musgitstalk.js

--- plugins/ezgitstalk.js
+++ plugins/ezgitstalk.js
@@ -27,7 +27,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 👥 *Followers*: ${data.followers} | Following: ${data.following}
 📅 *Created At*: ${new Date(data.created_at).toDateString()}
 🔭 *Public Gists*: ${data.public_gists}
-> © FREDI 𝘔𝘋`;
+> © MUSTAFFA MK`;
           const sentMsg = await conn.sendMessage(from,{image:{url: data.avatar_url },caption: userInfo },{quoted:mek })
     } catch (e) {
         console.log(e);
@@ -35,4 +35,4 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
     }
 });
 
-// FREDI 𝘔𝘋
+// MUSTAFFA MK
